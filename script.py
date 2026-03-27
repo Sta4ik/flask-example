02b4940620dbe3c09e97a495e5caf55debe6ee42
Sta4ik <sta4ik@tutamail.com>
2026-03-27 19:07:19 +0300

    add session

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///first.db'
 db = SQLAlchemy(app)
+app.secret_key = 'sqfdfsdg'
+app.permanent_session_lifetime = timedelta(minutes=1)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,10 +31,10 @@ def reg():
             
             return redirect('/login')
         except:
-            return "Ошибка"
+            return render_template('reg.html', error="Данный пользователь уже существует")
     else:
         return render_template('reg.html')
-    
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == "POST":
@@ -41,19 +44,32 @@ def login():
         user = Users.query.filter_by(login=login).first()
         
         if not user:
-            return "Пользователя не существует"
+            return render_template('login.html', error="Пользователя не существует")
 
         if user.password != password:
-            return "Ошибка"
+            return render_template('login.html', error="Неверный пароль")
         else:
+            session.permanent = True
+            session['login'] = login
             return redirect('/main')
     else:
+        if 'login' in session:
+            return redirect('/main')
+        
         return render_template('login.html')
-    
+
 @app.route('/main')
-def main(login):
-    return render_template('main.html', login=login)
-    
+def main():
+    if 'login' not in session:
+        return redirect('/login')
+
+    return render_template('main.html', login=session['login'])
+
+@app.route('/logout')
+def logout():
+    session.pop('login', None)
+    return redirect('/login')
+
 if __name__ == '__main__':
     app.run(debug=True)
     db.create_all()
